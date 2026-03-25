d3a9328f1d1f3d2139785725b0c3d064a627e5be
Zawe Zaw Htet <doomsday77923@gmail.com>
2026-03-25 16:52:52 +0900

    Fix remaining broken </title>title> tag in index.php

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ function getItemImage($itemId) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SmartOrder - Table <?php echo (int)$tableNo; ?></title>title>
+    <title>SmartOrder - Table <?php echo (int)$tableNo; ?></title>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
 <style>
     *{margin:0;padding:0;box-sizing:border-box}
