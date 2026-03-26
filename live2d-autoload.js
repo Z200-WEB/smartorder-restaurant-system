0662fe0d62de049f8d0281ff3db077ff05d1e6fc
Zawe Zaw Htet <doomsday77923@gmail.com>
2026-03-27 04:07:49 +0900

    fix: use stable v1.0.0 CDN paths for waifu-tips.js (RC version was broken)

--- live2d-autoload.js
+++ live2d-autoload.js
@@ -1,8 +1,8 @@
 /*!
- * Live2D Widget - Custom Japanese Tips Loader
- * Based on: https://github.com/stevenjoezhang/live2d-widget
+ * Live2D Widget - Custom Japanese Tips
+ * Mirror of live2d-widgets@1.0.0/dist/autoload.js with custom waifuPath
  */
-const live2d_path = 'https://fastly.jsdelivr.net/npm/live2d-widgets@1.0.0-rc.6/dist/';
+const live2d_path = 'https://fastly.jsdelivr.net/npm/live2d-widgets@1.0.0/dist/';
 
 function loadExternalResource(url, type) {
   return new Promise((resolve, reject) => {
@@ -29,8 +29,6 @@ function loadExternalResource(url, type) {
     loadExternalResource(live2d_path + 'waifu.css', 'css'),
     loadExternalResource(live2d_path + 'waifu-tips.js', 'js')
   ]);
-
-  // waifu-tips.js sets window.initWidget
   window.initWidget({
     waifuPath: '/waifu-tips.json',
     cdnPath: 'https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/',
